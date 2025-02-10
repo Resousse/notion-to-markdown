73b8d6c971a5a83694c75485f0112f04a6a0c400
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2025-02-10 10:49:40 -0800

    Fix warnings

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { Client } = require('@notionhq/client');
 const { NotionToMarkdown } = require('notion-to-md');
-const fetch = require('node-fetch');
 const fs = require('fs').promises;
 
 const notionClient = new Client({ auth: process.env['INPUT_NOTION-TOKEN'] });
@@ -13,9 +12,9 @@ const notionToMarkdown = new NotionToMarkdown({ notionClient });
   search.results.forEach (async result => {
     const baseName = result.url.split('/').pop();
 
-    const blocks = await notionToMarkdown.pageToMarkdown(result.id);
+    const mdBlocks = await notionToMarkdown.pageToMarkdown(result.id);
 
-    const { parent } = notionToMarkdown.toMarkdownString(mdBlocks = blocks);
+    const { parent } = notionToMarkdown.toMarkdownString(mdBlocks);
 
     if(typeof parent !== "undefined") {
       await fs.writeFile(`${baseName}.md`, parent);
